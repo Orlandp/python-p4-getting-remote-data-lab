82929d8b65a7ffd1ee964e4c4f25beb9df284ba9
pknowledge <jamesblssings@gmail.com>
2025-09-18 14:21:35 +0300

    remote

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,19 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        """
+        Sends a GET request to the URL provided during initialization
+        and returns the raw response body as text.
+        """
+        response = requests.get(self.url)
+        response.raise_for_status()  # raise exception if status code is 4xx/5xx
+        return response.text
+
 
     def load_json(self):
-        pass
+         """
+        Sends a GET request, retrieves the response body,
+        and parses it into a Python object (list or dict).
+        """
+         response_body = self.get_response_body()
+         return json.loads(response_body)
